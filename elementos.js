8e0162a880c29c48dae14a681f477cd54f967b7a
Jose Estrada <j.estrada49@icloud.com>
2020-02-19 10:37:58 -0700

    createElements

---
+++ elementos.js
@@ -0,0 +1,54 @@
+document.body.createElement('div').addClass('calculadora')
+
+document.createElement('div').addClass('pantalla')
+document.createElement('div')
+document.getElementById('calculadora').appendChild(pantalla)
+document.getElementById('calculadora').appendChild(botones)
+
+
+
+
+var textoBtns = Array['AC', 'DEL', '+/-', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=']
+
+/*for (let i = 0; i < textoBtns.lenght; i++) {
+  boton = document.getElementsByClassName('botones').createElement('div')
+  boton.textContent = textoBtns
+  document.getElementsByClassName('botones').appendChild(boton)
+}*/
+
+
+
+/*
+formulario = (document.getElementByName("frmCalc"))
+
+formulario.innerHTML = '<div class="pantalla"></div>'
+formulario.innerHTML = '<div class="botones"></div>'
+
+pantalla = document.getElementsByClassName("pantalla")
+botones = document.getElementsByClassName("botones")
+
+
+
+pantalla.innerHTML = '<div data-operacion-anterior class="operacionAnterior"></div>'
+pantalla.innerHTML = '<div data-operacion-actual class="operacionActual"></div>'
+
+// botones
+botones.innerHTML = '<div data-limpiar>AC</div>'
+botones.innerHTML = '<div data-eliminar>DEL</div>'
+botones.innerHTML = '<div>+/-</div>'
+botones.innerHTML = '<div data-operador>÷</div>'
+botones.innerHTML = '<div data-numero>7</div>'
+botones.innerHTML = '<div data-numero>8</div>'
+botones.innerHTML = '<div data-numero>9</div>'
+botones.innerHTML = '<div data-operador>×</div>'
+botones.innerHTML = '<div data-numero>4</div>'
+botones.innerHTML = '<div data-numero>5</div>'
+botones.innerHTML = '<div data-numero>6</div>'
+botones.innerHTML = '<div data-operador>-</div>'
+botones.innerHTML = '<div data-numero>1</div>'
+botones.innerHTML = '<div data-numero>2</div>'
+botones.innerHTML = '<div data-numero>3</div>'
+botones.innerHTML = '<div data-operador>+</div>'
+botones.innerHTML = '<div data-numero id="doble">0</div>'
+botones.innerHTML = '<div data-numero>.</div>'
+botones.innerHTML = '<div data-igual>=</div>'*/
